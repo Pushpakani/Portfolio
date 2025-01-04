65fe0c4946488ed267c4530aa70c368ab3775ace
Pushpakani <pushpakani597321@gmail.com>
2025-01-04 17:36:06 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,19 @@
 import Image from 'react-bootstrap/Image';
 import React from "react";
 import myimage from "../images/profile.png";
+import "../App.css"
 
 function About() {
     return (
 
-        <section className='d-flex flex-row justify-content-center align-items-center  min-vh-100' id="about">
-
-
-            <div className='m-5'>
-                <h1 className='d-flex justify-content-center'>About</h1>
-                <p>A highly skilled and passionate React Developer with a user-friendly,
+        <section className="row g-0 text-center align-items-center mb-5 min-vh-100" id="about">
+            <div className="col-sm-6 col-md-8 justify-text-content">
+                <h1 className="text-center">About</h1>
+                <p className="justify">A highly skilled and passionate React Developer with a user-friendly,
                     and high-performance web applications using modern technologies. Adept at working with React.js, Redux, JavaScript (ES6+), and RESTful APIs, with a solid understanding of front-end design patterns, state management, and UI/UX principles. Proven track record of delivering robust, scalable solutions in agile environments. Constantly improving skills and keeping up-to-date with the latest web development trends and tools.</p>
             </div>
-            <div>
-                <Image src={myimage} roundedCircle />
-
+            <div className="col-sm-6 col-md-4">
+                <Image src={myimage} roundedCircle className="img-fluid" alt="Profile Image" />
             </div>
         </section>
     )
